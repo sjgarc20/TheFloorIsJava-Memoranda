50971dc0bdc0e09890d4d99fa2246a6816764181
David Berry <davidjohnberryjr@gmail.com>
2023-06-02 16:42:27 -0700

    Berry 1: Changed version number to 'David Berry'

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -35,7 +35,10 @@ public class App {
        The actual values are substituted by the Ant build script using 
        'version' property and datestamp.*/
 
-	public static final String VERSION_INFO = "@VERSION@";
+	// public static final String VERSION_INFO = "@VERSION@";
+	// public static final String BUILD_INFO = "@BUILD@";
+	
+	public static final String VERSION_INFO = "David Berry";
 	public static final String BUILD_INFO = "@BUILD@";
 	
 	/*========================================================================*/
